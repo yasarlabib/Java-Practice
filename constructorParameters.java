e1df7053a69621d4d86adb2e50e0f4d8916d61fc
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-11-22 14:23:10 -0600

    Create constructorParameters.java

---
+++ constructorParameters.java
@@ -0,0 +1,16 @@
+public class Store {
+  // instance fields
+  String productType;
+  
+  // constructor method
+  public Store(String product) {
+    //set the instance field equal to paremeter of constructor 
+    productType = product;
+  }
+  
+  // main method
+  public static void main(String[] args) {
+    
+    
+  }
+}
