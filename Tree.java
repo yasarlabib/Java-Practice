85a02bf9da223659df3ef854e2f8126ee837e1eb
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-11-23 21:43:11 -0600

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree{
+  public static void main(String[] args){
+    //introduce ourselves and print a tree
+    System.out.println("Hey there, I'm Yasar and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("   *  ");
+    System.out.println("  *** ");
+    System.out.println(" ***** ");
+    System.out.println("   *  ");
+    System.out.println("   *  ");   
+  }
+}
