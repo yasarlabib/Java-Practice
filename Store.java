9242c9949cabe94c07f46c8be5de2189d2b12ebd
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-11-26 20:30:14 -0600

    Update Store.java
    called greetCustomer() method with a parameter

--- Store.java
+++ Store.java
@@ -1,24 +1,30 @@
 public class Store {
-  // instance fields
-  String productType;
+  	// instance fields
+  	String productType;
   
-  // constructor method
-  public Store(String product) {
-    productType = product;
+// constructor method
+public Store(String product) {
+    	productType = product;
   }
   
-  // advertise method
-  public void advertise() {
-		System.out.println("Selling " + productType + "!");
-    System.out.println("Come spend some money!");
+// advertise method
+public void advertise() {
+	System.out.println("Selling " + productType + "!");
+    	System.out.println("Come spend some money!");
+  }
+
+public void greetCustomer(String customer){
+	System.out.println("Welcome to the store, " + customer + "!");
   }
   
-  // main method
-  public static void main(String[] args) {
-    Store lemonadeStand = new Store("Lemonade");
-    //calling the advertise() method 3 times
-    lemonadeStand.advertise();
-    lemonadeStand.advertise();
-    lemonadeStand.advertise();
+// main method
+public static void main(String[] args) {
+	Store lemonadeStand = new Store("Lemonade");
+	//calling the advertise() method 3 times
+	lemonadeStand.advertise();
+	lemonadeStand.advertise();
+	lemonadeStand.advertise();
+	//calling greetCustomer() method with a parameter
+	lemonadeStand.greetCustomer("Yasar");
   }
 }
