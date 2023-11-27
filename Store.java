ee78db00f6e805f6585116299c349c063b29364e
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-11-26 21:00:22 -0600

    Update Store.java
    created a method that returns the price of the product with tax

--- Store.java
+++ Store.java
@@ -14,7 +14,7 @@ public void advertise() {
     	System.out.println("Come spend some money!");
   }
 
-//greet customer method
+// greet customer method
 public void greetCustomer(String customer){
 	System.out.println("Welcome to the store, " + customer + "!");
   }
@@ -24,17 +24,25 @@ public void greetCustomer(String customer){
     double newPrice = price + priceToAdd;
     price = newPrice;
   }
+
+// get price with tax method
+  public double getPriceWithTax(){
+    double totalPrice = price + price*0.08;
+    return totalPrice;
+  }
   
 // main method
 public static void main(String[] args) {
-	Store lemonadeStand = new Store("Lemonade");
-	//calling the advertise() method 3 times
+	Store lemonadeStand = new Store("Lemonade", 3.75);
+	// calling the advertise() method 3 times
 	lemonadeStand.advertise();
 	lemonadeStand.advertise();
 	lemonadeStand.advertise();
-	//calling greetCustomer() method with a parameter
+	// calling greetCustomer() method with a parameter
 	lemonadeStand.greetCustomer("Yasar");
-	//increase price of product by 1.5
+	// increase price of product by 1.5
 	lemonadeStand.increasePrice(1.5);
+	// sets the price of lemonade including tax
+	double lemonadePrice = lemonadeStand.getPriceWithTax();
   }
 }
