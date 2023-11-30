e84f84911072fac42f5b15627eb66bce345c53bc
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-11-29 22:22:13 -0600

    Update Reservation.java
    added a conditional that prints "Invalid reservation" if these is more than 8 guests or less than 1 guest

--- Reservation.java
+++ Reservation.java
@@ -7,40 +7,43 @@ public class Reservation {
 
     // constructor for the Reservation class
     public Reservation(int count, int capacity, boolean open) {
-      guestCount = count;
-  		restaurantCapacity = capacity;
-  		isRestaurantOpen = open;
+            if (count < 1 || count > 8) {
+                  System.out.println("Invalid reservation!");
+            }
+            guestCount = count;
+  		    restaurantCapacity = capacity;
+  		    isRestaurantOpen = open;
     }  
 
     // the confirmReservation() method checks to see:
     public void confirmReservation() {
-      // if restaurantCapacity is greater or equal to guestCount AND the restaurant is open
-        if(restaurantCapacity >= guestCount && isRestaurantOpen){
-          // print "Reservation confirmed"
-          System.out.println("Reservation Confirmed");
-          // set isConfirmed to true
-          isConfirmed = true;
-        } else{
-          // print "Reservation denied"
-          System.out.println("Reservation denied");
-          // set isConfirmed to false
-          isConfirmed = false;
-        }
+            // if restaurantCapacity is greater or equal to guestCount AND the restaurant is open
+            if(restaurantCapacity >= guestCount && isRestaurantOpen){
+            // print "Reservation confirmed"
+            System.out.println("Reservation Confirmed");
+            // set isConfirmed to true
+            isConfirmed = true;
+            } else{
+            // print "Reservation denied"
+            System.out.println("Reservation denied");
+            // set isConfirmed to false
+            isConfirmed = false;
+            }
     }
 
     // The informUser() method will always print the statement for now (will be fixed later)
     public void informUser() {
-        System.out.println("Please enjoy your meal!");
+            System.out.println("Please enjoy your meal!");
     }
   
     public static void main(String[] args) {
-        // creating objects for the Reservation class
-        Reservation partyOfThree = new Reservation(3, 12, true);
-        Reservation partyOfFour = new Reservation(4, 3, true);
-        calling methods on the objects 
-        partyOfThree.confirmReservation();
-        partyOfThree.informUser();
-        partyOfFour.confirmReservation();
-        partyOfFour.informUser();
-    }
+            // creating objects for the Reservation class
+            Reservation partyOfThree = new Reservation(3, 12, true);
+            Reservation partyOfFour = new Reservation(4, 3, true);
+            calling methods on the objects 
+            partyOfThree.confirmReservation();
+            partyOfThree.informUser();
+            partyOfFour.confirmReservation();
+            partyOfFour.informUser();
+        }
 }
