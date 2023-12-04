a97bb7127aca851a00780ed218b382de4d8c782b
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-12-04 14:05:24 -0600

    Create NewsFeed.java

---
+++ NewsFeed.java
@@ -0,0 +1,21 @@
+public class Newsfeed {
+
+    // constructor for the NewsFeed class 
+    public Newsfeed(){
+    }
+
+    // getTopics will return an array of Strings that holds the topics 
+    public String[] getTopics(){
+        String[] topics = {"Opinion", "Tech", "Science", "Health"};
+        return topics;
+    }
+
+    public static void main(String[] args){
+        // creating NewsFeed object 
+        Newsfeed sampleFeed = new Newsfeed();
+        // creating an array that holds the topics 
+        String[] topics = sampleFeed.getTopics();
+        // printing out the topics 
+        System.out.println(topics);
+    }
+}
