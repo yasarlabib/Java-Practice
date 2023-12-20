687bebe8cbd2e60964a9af58e1cf787150e87155
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-12-20 11:07:31 -0600

    Update ATM.java
    Modified Static Variables

--- ATM.java
+++ ATM.java
@@ -9,24 +9,42 @@ public class ATM{
     // constructor for the ATM class 
     public ATM(int inputMoney){
         this.money = inputMoney;
+        // static variable keeps track of how many ATMs there are
+        numATMs++;
+        // static variable keeps track of total money in the system 
+        totalMoney += inputMoney;
     }
   
     // withdraw() method lets user withdraw money from the ATM
     public void withdrawMoney(int amountToWithdraw){
         if(amountToWithdraw <= this.money){
             this.money -= amountToWithdraw;
+            // removes money from the static variable totalMoney
+            totalMoney -= amountToWithdraw;
         }
     }
   
     // main method 
     public static void main(String[] args){
+        // printing number of ATMs before creating new ATMs
+        System.out.println("Total number of ATMs: " + ATM.numATMs); 
         // creating two ATMs
         ATM firstATM = new ATM(1000);
         ATM secondATM = new ATM(500);
+        // printing number of ATMs after creating new ATMs
+        System.out.println("Total number of ATMs: " + ATM.numATMs); 
+
+        // printing total money in the system before withdrawing money
+        System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);  
+        // withdrawing money using the withdrawMoney() method
+        firstATM.withdrawMoney(500);
+        secondATM.withdrawMoney(200);
+        // printing total money in the system after withdrawing money
+        System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);    
     
         // printing your static variable in three different ways here
-        System.out.println(ATM.totalMoney); // prints 0
-        System.out.println(firstATM.totalMoney); //prints 0
-        System.out.println(secondATM.totalMoney); // prints 0
+        System.out.println(ATM.totalMoney); // prints 800
+        System.out.println(firstATM.totalMoney); // prints 800
+        System.out.println(secondATM.totalMoney); // prints 800
     }
 }
